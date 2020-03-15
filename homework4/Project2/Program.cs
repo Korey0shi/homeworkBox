b84fe705783cb724eff31af218f1d2e62593953d
ZwZ <15326631213@qq.com>
2020-03-15 13:52:52 +0800

    1

---
+++ homework4/Project2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Project2
+{
+    class Clock
+    {
+        public event AlarmEventHandle AlarmEvent;
+        public event TickEventHandle TickEvent;
+        private int alarmHour;
+        private int alarmMinute;
+        private int alarmSecond;
+
+        public void SetAlarm(int alarmHour,int alarmMinute,int alarmSecond)
+        {
+            this.alarmHour = alarmHour;
+            this.alarmMinute = alarmMinute;
+            this.alarmSecond = alarmSecond;
+        }
+        public void StartWork()
+        {
+            while (true)
+            {
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+                this.TickEvent(hour, minute, second);
+                if(hour==this.alarmHour&&minute==this.alarmMinute&&second==this.alarmSecond)
+                {
+                    AlarmEvent(hour, minute, second);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Clock myClock = new Alarm.Clock();
+            myClock.AlarmEvent += ClockAlarm;
+            myClock.TickEvent += ClockTick;
+            myClock.SetAlarm(10, 12, 50);
+            myClock.StartWork();
+        }
+    }
+ 
+}
---
+++ homework4/homework4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Project1
+{
+    public class Node<T>
+    {
+        public Node<int> Next { get; set; }
+        public int Data { get; set; }
+
+        public Node(int t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<int> head;
+        private Node<int> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<int> Head
+        {
+            get => head;
+        }
+        public void Add(int t)
+        {
+            Node<int> n = new Node<int>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+            List<int> list = new List<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                list.Add(x);
+            }
+            int sum = 0;
+            list.ForEach(x => Console.WriteLine(x));
+            list.ForEach(delegate (int x) { sum += x; });
+            list.ForEach(i => sum += i);
+        }
+    }
+
+}
